d75be7720bd4327afb5cb386f25a28e815855d5f
Aleksandra Wilk <aleksandra_wilk@yahoo.com>
2025-05-12 13:16:03 +0200

    add validations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,10 @@
 class Bookmark < ApplicationRecord
   belongs_to :recipe
   belongs_to :category
+
+  validates :comment, length: { minimum: 6 }
+  # bookmark must be linked to a recipe and a category, and the [recipe, category] pairings should be unique
+  validates :recipe_id, uniqueness: { scope: :category_id }
+  # scope groups them - makes a "pairing"/association, that's why order doesn't matter:
+  # ^same as = validates :category_id, uniqueness: { scope: :recipe_id }
 end
